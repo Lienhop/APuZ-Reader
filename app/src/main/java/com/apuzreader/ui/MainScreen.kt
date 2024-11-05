a232fd7d280675d5223fc75687b2ec05587338fd
Lienhop <hans.lienhop@gmail.com>
2024-11-05 12:03:58 +0100

    Added "Favorites" Functionality
    This includes a navigation drawer for selecting the favorites view, an isFavorite property for ApuzData and opening a modal rather than directly opening the pdf upon clicking on an apuz image.

--- app/src/main/java/com/apuzreader/ui/MainScreen.kt
+++ app/src/main/java/com/apuzreader/ui/MainScreen.kt
@@ -1,107 +1,170 @@
 package com.apuzreader.ui
 
-import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.apuzreader.models.ApuzData
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 enum class SortOrder { NEWEST, OLDEST }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
     pdfList: List<ApuzData>,
-    onLaunchPdf: (String, String) -> Unit
+    onLaunchPdf: (String, String) -> Unit,
+    onToggleFavorite: (ApuzData) -> Unit,
+    drawerState: DrawerState
 ) {
+    var showFavorites by remember { mutableStateOf(false) }
     var searchQuery by remember { mutableStateOf("") }
-    var sortOrder by remember { mutableStateOf(SortOrder.NEWEST) }
+    val scope = rememberCoroutineScope()
+    val sortOrder by remember { mutableStateOf(SortOrder.NEWEST) }
 
-    // Add this for debugging
-    LaunchedEffect(Unit) {
-        Log.d("MainScreen", "Composable recomposed. PDF list size: ${pdfList.size}")
-    }
-
-    Column(modifier = modifier.fillMaxSize()) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            OutlinedTextField(
-                value = searchQuery,
-                onValueChange = {
-                    searchQuery = it
-                    Log.d("MainScreen", "Search query changed to: $it")
-                },
-                modifier = Modifier.weight(1f),
-                placeholder = { Text("Suchen") },
-                leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) }
-            )
-//            Spacer(modifier = Modifier.width(8.dp))
-//            IconButton(
-//                onClick = {
-//                    sortOrder = when (sortOrder) {
-//                        SortOrder.NEWEST -> SortOrder.OLDEST
-//                        SortOrder.OLDEST -> SortOrder.NEWEST
-//                    }
-//                    Log.d("MainScreen", "Sort order changed to: $sortOrder")
-//                }
-//            ) {
-//                Icon(
-//                    imageVector = when (sortOrder) {
-//                        SortOrder.NEWEST -> Icons.Default.KeyboardArrowDown
-//                        SortOrder.OLDEST -> Icons.Default.KeyboardArrowUp
-//                    },
-//                    contentDescription = when (sortOrder) {
-//                        SortOrder.NEWEST -> "Sort by newest"
-//                        SortOrder.OLDEST -> "Sort by oldest"
-//                    }
-//                )
-//            }
-        }
-
-        val filteredAndSortedList by remember(pdfList, searchQuery) {
-            derivedStateOf {
-                Log.d("MainScreen", "Recalculating filtered and sorted list")
-                pdfList
-                    .filter {
-                        it.title.contains(searchQuery, ignoreCase = true) ||
-                                it.description.contains(searchQuery, ignoreCase = true)
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerContent(
+                onShowAllClick = {
+                    showFavorites = false
+                    scope.launch {
+                        drawerState.close()
                     }
+                },
+                onShowFavoritesClick = {
+                    showFavorites = true
+                    scope.launch {
+                        drawerState.close()
+                    }
+                }
+            )
+        },
+        content = {
+            Column(modifier = modifier.fillMaxSize()) {
+                // Top App Bar
+                TopAppBar(
+                    title = { Text(if (showFavorites) "Favoriten" else "Übersicht") },
+                    navigationIcon = {
+                        IconButton(onClick = { scope.launch {
+                            drawerState.open()
+                        } }) {
+                            Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                        }
+                    }
+                )
+
+                // Search bar
+                OutlinedTextField(
+                    value = searchQuery,
+                    onValueChange = { searchQuery = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp, start = 16.dp, end = 16.dp, bottom = 16.dp),
+                    placeholder = { Text("Suchen") },
+                    leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) }
+                )
+
+                val displayList = pdfList
                     .sortedWith(when (sortOrder) {
                         SortOrder.NEWEST -> compareByDescending { it.date ?: LocalDate.MIN }
                         SortOrder.OLDEST -> compareBy { it.date ?: LocalDate.MAX }
                     })
-                    .also { Log.d("MainScreen", "Filtered list size: ${it.size}") }
+                    .filter { if (showFavorites) it.isFavorite else true }
+                    .filter {
+                        it.title.contains(searchQuery, ignoreCase = true) ||
+                                it.description.contains(searchQuery, ignoreCase = true)
+                    }
+
+                if (showFavorites && displayList.isEmpty()) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            "Fügen Sie Favoriten hinzu, um sie hier zu sehen.",
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                }
+                else{
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 16.dp)
+                    ) {
+                        items(displayList) { pdfData ->
+                            ApuzEdition(
+                                title = pdfData.title,
+                                description = pdfData.description,
+                                imageUrl = pdfData.imageUrl,
+                                date = pdfData.date,
+                                onReadPdf = { onLaunchPdf(pdfData.pdfUrl, pdfData.title) },
+                                onToggleFavorite = { onToggleFavorite(pdfData) },
+                                isFavorite = pdfData.isFavorite,
+                                showFavorites = showFavorites
+                            )
+                        }
+                    }
+                }
             }
         }
+    )
+}
 
-        LazyColumn(
+@Composable
+fun DrawerMenuItem(text: String, onClick: () -> Unit) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
+        fontSize = 22.sp,
+    )
+}
+
+@Composable
+fun DrawerContent(
+    onShowAllClick: () -> Unit,
+    onShowFavoritesClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(0.45f)  // Fill only a third of the screen width
+            .background(MaterialTheme.colorScheme.surface.copy(alpha = 0.95f))  // Opaque background
+    ) {
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 16.dp)
+                .padding(top = 40.dp, start = 16.dp, end = 16.dp, bottom = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
-            items(filteredAndSortedList) { pdfData ->
-                ApuzEdition(
-                    title = pdfData.title,
-                    description = pdfData.description,
-                    imageUrl = pdfData.imageUrl,
-                    date = pdfData.date,
-                    onClick = { onLaunchPdf(pdfData.pdfUrl, pdfData.title) }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-            }
+            Text(
+                text = "APuZ Reader",
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(bottom = 2.dp)
+            )
+
+            // Divider
+            HorizontalDivider(color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f))
+            DrawerMenuItem("Übersicht", onShowAllClick)
+            DrawerMenuItem("Favoriten", onShowFavoritesClick)
         }
     }
 }
